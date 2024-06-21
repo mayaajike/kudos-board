23c00f14040a925ec39388d2839518cf76bc97aa
Maya Ody-Ajike <mayaajike@meta.com>
2024-06-21 01:36:12 -0700

    footer edits

--- Frontend/kudos-board-frontend/src/Components/Footer.jsx
+++ Frontend/kudos-board-frontend/src/Components/Footer.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import '../App.css'
+import '../App.css';
+import { FiInstagram } from "react-icons/fi";
+import { CiFacebook } from "react-icons/ci";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { FaPhone } from "react-icons/fa";
+
 
 
 const Footer = () => {
     return (
         <footer>
-            <p>Cppyright &copy; 2024 Meta U</p>
+            <div className='footer-about'>
+                <p>
+                    <h3>About</h3>
+                    <span>Celebrating the power of appreciation.</span>
+                    <span>Share gratitude, recognition, and kudos with your team.</span>
+                    <span>Building stronger relationships and a positive work culture, one thank you at a time.</span>
+                </p>
+            </div>
+            <div>
+                <h3>Contact</h3>
+                <div className='icons-container'>
+                    <FiInstagram className='icons'/> @kudos
+                    <CiFacebook className='icons'/> @kudos
+                    <MdOutlineMailOutline className='icons'/> <a href="mailto:info@kudos.com">info@kudos.com</a>
+                    <FaPhone className='icons' /> +1-(234)-5678910
+                </div>
+            </div>
+            <p>Copyright &copy; 2024 Meta U</p>
         </footer>
     )
 }
